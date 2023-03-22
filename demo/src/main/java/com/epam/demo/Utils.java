130b4f8bd4ab5887242e9205ded8cea90b4df35d
Vladislav Samorukov <smrk.vld@gmail.com>
2023-03-23 00:56:00 +0300

    done

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,16 @@
 package com.epam.demo;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        for (String string : args) {
+            if (!StringUtils.isNumeric(string) || Integer.parseInt(string) <= 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
